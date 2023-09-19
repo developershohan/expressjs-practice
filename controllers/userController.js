e1754afa866eb153f354ec9cbe526964d53837c4
Developer Shohan <dev.shohanur@gmail.com>
2023-09-20 00:28:52 +0600

    email send fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,29 @@
 import nodemailer from "nodemailer"
 
-export const registerUser = async (req,res)=>{
-
+export const registerUser = async (req, res) => {
+    // create mail transport
     const transport = nodemailer.createTransport({
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
-        auth:{
-            user: process.env.MAIL_ADDRESS,
-            pass: process.env.MAIL_PASS,
+        auth: {
+            user : process.env.MAIL_ADRESS,
+        pass: process.env.MAIL_PASS,
 
-        },
-    });
+        }
+    })
+     await   transport.sendMail({
+        from: `JakaNaka Group <${process.env.MAIL_ADRESS}>`,
+        subject: "JakaNaka Test",
+        to: req.body.email,
+       text: `hello ${req.body.name},
+  
+       You are ${req.body.age} Years old
 
-await transport.sendMail({
-    from:`shohan LTD <${process.env.MAIL_ADDRESS}>`,
-    subject: "test email",
-    to: req.body.email,
-    text: `Hello ${req.body.name},
-    <br>
-    welcome, you are ${req.body.age} years old
+       `
+        }),
 
-    `
-})
 
-    res.status(200).json(req.body)
+       
+       
+        res.status(200).json(req.body)
 }
